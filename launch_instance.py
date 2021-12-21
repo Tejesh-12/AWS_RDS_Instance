73dbee4fbc05c5547dfcda33fa3558e43987b957
Tejesh-12 <89392868+Tejesh-12@users.noreply.github.com>
2021-12-21 15:31:55 +0530

    Update launch_instance.py

--- launch_instance.py
+++ launch_instance.py
@@ -1,11 +1,9 @@
 import boto3
 
 
-access_id_key = "ASIAWTQ2NIW57NQOSBUM"
-secret_access_key = "rcmiIg/nBKI/P574YF/tjz2P2b2j4AP5DDmpp4BO"
-session_token_key = "FwoGZXIvYXdzEKr//////////wEaDM8dXbrKwomP5FS0hyLLAc18S0oQIsUS7OBmS9TzV281pcKaOYf1kEc2J+G6ICmZLuQj4IhcI1p9NyQngSF2CFq+JIQjqdhZ6X9m2mvIE9ZhQriDIwx1R7SmPwq22IeBF7+tBKGuP7Nden2IrwSVYK8PCIcxwgw4xt2whP63jCISbQ0QgF6apt6Zlr2rW5dbg0CEiQKrJGl8SnewtIU2lcpK9seiPnYTCE2Yl0BMev94JDy8M8nDUmgy3C4Tk0G7r7tPWQTdAbPT7J8P0CgIlOh6QZvOsmEOx1QSKO+LqIoGMi0VtX3Bi6nhDSZoDZmTgC6HTAXznYNV+t07TXfFQSCL8ID/s6hLRUCNZuMXCmA="
-
-
+access_id_key = ""
+secret_access_key = ""
+session_token_key = ""
 ec2 = boto3.resource(
     "ec2",
     aws_access_key_id=access_id_key,
@@ -21,7 +19,7 @@ instances = ec2.create_instances(
     InstanceType="t2.micro",
     KeyName="Cs351-2021",
     SecurityGroupIds=["sg-00b8d4fc8d0362232"],
-    UserData=open("C:/Users/bhara/Downloads/script (1).sh").read(),
+    UserData=open("C:/Users/tejes/Downloads/script.sh").read(),
 )
 
 
